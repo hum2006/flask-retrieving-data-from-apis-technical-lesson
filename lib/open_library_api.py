860a6423d3f0ca63e0aaad4c04843bae4cf9f63d
HUM <alubokho24.nandwa@student.cuk.ac.ke>
2026-09-24 11:24:17 +0300

    done

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -1,1 +1,30 @@
+import requests
 
+
+class Search:
+    def get_search_results(self, search_term):
+        URL = "https://openlibrary.org/search.json"
+
+        response = requests.get(
+            URL,
+            params={
+                "title": search_term,
+                "fields": "title,author_name",
+                "limit": 1
+            }
+        ).json()
+
+        response_formatted = (
+            f"Title: {response['docs'][0]['title']}\n"
+            f"Author: {response['docs'][0]['author_name'][0]}"
+        )
+
+        return response_formatted
+
+
+search_term = input("Enter a book title: ")
+
+result = Search().get_search_results(search_term)
+
+print("Search Result:\n")
+print(result)
